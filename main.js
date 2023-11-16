c69ac708fb0052f3b982bef9f31f90b8a2fdc753
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-16 15:13:10 +0100

    base di partenza

---
+++ main.js
@@ -0,0 +1,27 @@
+const slides = [
+    {
+    immagine:
+    'https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea
+    -with-coconut-palm-tree_74190-1075.jpg',
+    titolo: 'Tramonto sulla spiaggia',
+    descrizione: 'Un meraviglioso tramonto con colori caldi
+    che si riflettono sull\'acqua.'
+    },
+    {
+    immagine:
+    'https://img.freepik.com/free-photo/beautiful-shot-mountains-trees
+    -covered-snow-fog_181624-17590.jpg',
+    titolo: 'Montagne innevate',
+    descrizione: 'Un panorama mozzafiato delle montagne
+    coperte di neve.'
+    },
+    {
+    immagine:
+    'https://img.freepik.com/free-photo/countryside-field-sunny-day-co
+    untryside_181624-24010.jpg',
+    titolo: 'Campi verdi',
+    descrizione: 'Vaste distese di campi verdi che si
+    estendono all\'orizzonte.'
+    }
+    ];
+        
