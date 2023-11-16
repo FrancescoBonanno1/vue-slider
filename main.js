2814f8bf78861b59f5013bf804821faf411e9914
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-16 16:09:03 +0100

    problema con vue

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-const { createApp } = vue
 
+const {createApp} = Vue;
 createApp({
     data() {
         return {
             slides: [{
 
-                immagine: 'https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut - palm - tree_74190 - 1075.jpg',
+                immagine: 'https://my.alfred.edu/zoom/_images/foster-lake.jpg',
                 titolo: 'Tramonto sulla spiaggia',
                 descrizione: 'Un meraviglioso tramonto con colori caldi'
             },
             {
 
-                immagine: 'https://img.freepik.com/free-photo/beautiful-shot-mountains-trees- covered - snow - fog_181624 - 17590.jpg',
+                immagine: 'https://img.freepik.com/free-photo/beautiful-shot-mountains-trees-covered-snow-fog_181624-17590.jpg',
                 titolo: 'Montagne innevate',
                 descrizione: 'Un panorama mozzafiato delle montagne coperte di neve.'
             },
 
             {
-                immagine: 'https://img.freepik.com/free-photo/countryside-field-sunny-day-countryside_181624- 24010.jpg',
+                immagine: 'https://img.freepik.com/free-photo/countryside-field-sunny-day-countryside_181624-24010.jpg',
                 titolo: 'Campi verdi',
                 descrizione: 'Vaste distese di campi verdi che si estendono all\'orizzonte.'
             }
@@ -30,4 +30,4 @@ createApp({
         }
     }
 })
-    .mount(createApp);
+    .mount(`#app`);
